64865f6a9781892dba2c232b2eec5ef1d289342a
Coen <458416@student.fontys.nl>
2024-03-14 10:41:14 +0100

    Triggering workflow

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
 					  .AllowAnyHeader());
 });
 
-// Add services to the container.
+// Add services to the container. 
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
